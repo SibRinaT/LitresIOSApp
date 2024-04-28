f3c5737ab6e9f40205bf6e5cecec565ebb7480d9
Ainur <sibrinat616@gmail.com>
2024-04-28 15:25:45 +0300

    init

---
+++ PracticeAppSwiftUI/PracticeAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeAppSwiftUI
+//
+//  Created by Ainur on 28.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeAppSwiftUI/PracticeAppSwiftUI/PracticeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeAppSwiftUIApp.swift
+//  PracticeAppSwiftUI
+//
+//  Created by Ainur on 28.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct PracticeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
