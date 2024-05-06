ffc2dd018c1dd7082b5be3bf0a2e5e8d7a23be80
Ainur <sibrinat616@gmail.com>
2024-05-06 10:01:35 +0300

    button disabled  in LogInView

--- PracticeAppSwiftUI/PracticeAppSwiftUI/AuthentificationViews/LogInView.swift
+++ PracticeAppSwiftUI/PracticeAppSwiftUI/AuthentificationViews/LogInView.swift
@@ -12,6 +12,10 @@ struct LogInView: View {
     @State private var email = ""
     @State private var password = ""
     
+    var isLoginEnabled: Bool {
+        return (!password.isEmpty && !email.isEmpty)
+       }
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -58,13 +62,13 @@ struct LogInView: View {
                         Rectangle()
                             .frame(width: 224, height: 50)
                             .cornerRadius(16)
-                            .foregroundColor(Color("MainColor"))
-                            .overlay(
+                            .foregroundColor(isLoginEnabled ? Color("MainColor") : Color("InactiveColor").opacity(0.5))                            .overlay(
                                 Text("Вход")
                                     .foregroundColor(.white)
                                     .font(.custom("AmericanTypewriter", size: 20))
                             )
                     }
+                    .disabled(!isLoginEnabled)
                     
                 }
                 .foregroundColor(Color("MainColor"))
