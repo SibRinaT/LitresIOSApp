1f57772627ce04d390a680f67b15155b7305eb87
Artem Valiev <artem.valiev@gmail.com>
2024-05-08 17:03:47 +0530

    loading indicator for files loading

--- PracticeAppSwiftUI/PracticeAppSwiftUI/AdminViews/AdminFileUploadView.swift
+++ PracticeAppSwiftUI/PracticeAppSwiftUI/AdminViews/AdminFileUploadView.swift
@@ -10,39 +10,55 @@ import SwiftUI
 struct AdminFileUploadView: View {
     @Binding var fileName: String?
     @State private var isFileImporterPresented = false
+    @State private var isLoading = false
     
     var body: some View {
-        if let fileName {
-            HStack {
-                Text(fileName)
-                Spacer()
-                Button("Удалить") {
-                    deleteFile()
-                }
-                .foregroundColor(.red)
-            }
-        } else {
-            Button("Добавить файл") {
-                isFileImporterPresented = true
-            }
-            .fileImporter(isPresented: $isFileImporterPresented, allowedContentTypes: [.item]) { result in
-                switch result {
-                case .success(let url):
-                    uploadFileFrom(url: url)
-                case .failure(let failure):
-                    print(failure.localizedDescription)
+        VStack(alignment: .leading) {
+            if isLoading {
+                ProgressView()
+                    .progressViewStyle(.circular)
+            } else {
+                if let fileName {
+                    HStack {
+                        Button(fileName) {
+                            
+                        }
+                        .buttonStyle(.plain)
+                        Spacer()
+                        Button("Удалить") {
+                            deleteFile()
+                        }
+                        .foregroundColor(.red)
+                        .buttonStyle(.plain)
+                    }
+                } else {
+                    Button("Добавить файл") {
+                        isFileImporterPresented = true
+                    }
+                    .fileImporter(isPresented: $isFileImporterPresented, allowedContentTypes: [.item]) { result in
+                        switch result {
+                        case .success(let url):
+                            uploadFileFrom(url: url)
+                        case .failure(let failure):
+                            print(failure.localizedDescription)
+                        }
+                    }
                 }
             }
         }
     }
     
     private func uploadFileFrom(url: URL) {
+        isLoading = true
         Task {
             do {
                 let data = try Data(contentsOf: url)
                 let fileName = url.lastPathComponent
                 try await ImageStorage.shared.upload(data: data, path: "files/\(fileName)")
-                self.fileName = fileName
+                DispatchQueue.main.async {
+                    self.fileName = fileName
+                    self.isLoading = false
+                }
             } catch {
                 print(error)
             }
@@ -51,9 +67,13 @@ struct AdminFileUploadView: View {
     
     private func deleteFile() {
         guard let fileName else { return }
+        isLoading = true
         Task {
             try await ImageStorage.shared.deleteFileFrom(path: "files/\(fileName)")
-            self.fileName = nil
+            DispatchQueue.main.async {
+                self.fileName = nil
+                self.isLoading = false
+            }
         }
     }
 }
