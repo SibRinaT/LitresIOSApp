ce743bcba7c9d5d35fed945daa90ee4a891c05fc
Ainur <sibrinat616@gmail.com>
2024-05-18 11:10:43 +0300

    PlaysoundView added

---
+++ PracticeAppSwiftUI/PracticeAppSwiftUI/MainViews/PlaysoundView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaysoundView.swift
+//  PracticeAppSwiftUI
+//
+//  Created by Ainur on 18.05.2024.
+//
+
+import SwiftUI
+
+struct PlaysoundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaysoundView()
+}
