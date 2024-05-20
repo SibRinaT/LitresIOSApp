c559fef7d192fc0ae23cf31dadf6c7ddbef26489
Ainur <sibrinat616@gmail.com>
2024-05-20 14:15:56 +0300

    sibBuy view fixed

--- PracticeAppSwiftUI/PracticeAppSwiftUI/MainViews/SubsBuyView.swift
+++ PracticeAppSwiftUI/PracticeAppSwiftUI/MainViews/SubsBuyView.swift
@@ -2,18 +2,7 @@ import SwiftUI
 
 struct SubsBuyView: View {
     @State var showCardView = false
-    //    @State private var error: UploadError?
-    //    @State private var isLoading = false
-    
-    //    var isShowingError: Binding<Bool> {
-    //        Binding {
-    //            error != nil
-    //        } set: { _ in
-    //            error = nil
-    //        }
-    //    }
-    
-    
+
     var body: some View {
         NavigationView {
             ZStack {
@@ -28,8 +17,8 @@ struct SubsBuyView: View {
                         Spacer()
                     }
                     .padding()
-                    
-                    VStack {
+
+                    VStack(spacing: 16) {
                         Rectangle()
                             .frame(height: 250)
                             .cornerRadius(16)
@@ -43,13 +32,13 @@ struct SubsBuyView: View {
                                         .foregroundColor(Color(.white))
                                         .font(.custom("AmericanTypewriter", size: 28))
                                         .multilineTextAlignment(.center)
-                                    Text("150 руб в месяц")
+                                    Text("149 руб в месяц")
                                         .foregroundColor(Color(.white))
                                         .font(.custom("AmericanTypewriter", size: 20))
                                         .multilineTextAlignment(.center)
                                 }
                             )
-                        
+
                         Rectangle()
                             .foregroundColor(Color("SecondaryColor"))
                             .frame(height: 100)
@@ -62,7 +51,7 @@ struct SubsBuyView: View {
                                         .multilineTextAlignment(.center)
                                 }
                             )
-                        
+
                         Rectangle()
                             .foregroundColor(Color("SecondaryColor"))
                             .frame(height: 100)
@@ -73,17 +62,15 @@ struct SubsBuyView: View {
                                         .font(.custom("AmericanTypewriter", size: 14))
                                         .multilineTextAlignment(.center)
                                         .foregroundColor(Color("InactiveColor"))
-                                    
                                 }
                             )
                         Spacer()
                     }
                     .padding(.horizontal)
-                    
                     Spacer()
                 }
                 .padding()
-                
+
                 VStack {
                     Spacer()
                     Rectangle()
@@ -98,86 +85,55 @@ struct SubsBuyView: View {
                                         .font(.custom("AmericanTypewriter", size: 18))
                                     Spacer()
                                 }
-                                
-                                Rectangle()
-                                    .cornerRadius(16)
-                                    .foregroundColor(Color("InactiveColor"))
-                                
+
+                                Button(action: {
+                                    showCardView.toggle()
+                                }) {
+                                    Rectangle()
+                                        .fill(Color("InactiveColor"))
+                                        .cornerRadius(16)
+                                        .frame(width: 200, height: 57)
+                                        .overlay {
+                                            Text("Подключить")
+                                                .foregroundColor(.white)
+                                                .font(.custom("AmericanTypewriter", size: 24))
+                                                .bold()
+                                                .multilineTextAlignment(.center)
+                                        }
+                                }
                             }
-                                .padding(.horizontal)
-                        )
-                    Spacer()
-                }
-                .padding(.horizontal)
-                
-                Spacer()
-            }
-            
-            VStack {
-                Spacer()
-                Rectangle()
-                    .foregroundColor(Color("SecondaryColor"))
-                    .frame(height: 150)
-                    .ignoresSafeArea(.all)
-                    .overlay(
-                        VStack {
-                            HStack {
-                                Text("Сейчас вы платите - 149руб")
-                                    .foregroundColor(Color(.white))
-                                    .font(.custom("AmericanTypewriter", size: 18))
-                                Spacer()
-                            }
-                            
-                            Button (action: {
-                                showCardView.toggle()
-                            }) {
-                                Rectangle()
-                                    .fill(Color("InactiveColor"))
-                                    .cornerRadius(16)
-                                    .frame(width: 280, height: 57)
-                                    .overlay {
-                                        Text("Подключить")
-                                            .foregroundColor(.white)
-                                            .font(.custom("AmericanTypewriter", size: 24))
-                                            .bold()
-                                            .multilineTextAlignment(.center)
-                                    }
-                                
-                            }
-                            //                            .disabled(isLoading)
-                        }
                             .padding(.horizontal)
-                    )
+                        )
+                }
+                .ignoresSafeArea(edges: .bottom)
             }
-            .ignoresSafeArea(edges: .bottom)
         }
         .fullScreenCover(isPresented: $showCardView) {
             AddCardView()
         }
         //        .alert(isPresented: isShowingError, error: error) { _ in
-        //        } message: { error in
-        //            Text(error.errorDescription ?? "")
-        //        }
+           //        } message: { error in
+           //            Text(error.errorDescription ?? "")
+           //        }
     }
-    
     //    private func enableSub() {
-    //        isLoading = true
-    //        Task {
-    //            do {
-    //                try await authService.enableSubscription()
-    //                hideIndicator()
-    //            } catch {
-    //                self.error = UploadError.custom(text: error.localizedDescription)
-    //                hideIndicator()
-    //            }
-    //        }
-    //    }
-    
-    //    private func hideIndicator() {
-    //        DispatchQueue.main.async {
-    //            isLoading = false
-    //        }
-    //    }
+        //        isLoading = true
+        //        Task {
+        //            do {
+        //                try await authService.enableSubscription()
+        //                hideIndicator()
+        //            } catch {
+        //                self.error = UploadError.custom(text: error.localizedDescription)
+        //                hideIndicator()
+        //            }
+        //        }
+        //    }
+        
+        //    private func hideIndicator() {
+        //        DispatchQueue.main.async {
+        //            isLoading = false
+        //        }
+        //    }
 }
 
 #Preview {
